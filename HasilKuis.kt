15a2a3479181cf113f209f4f5e5f1a4b4eeddf40
Aqila Eling Febrianti <114634831+DP5-AqilaEling-012@users.noreply.github.com>
2024-10-27 08:36:12 +0700

    Update HasilKuis.kt

--- HasilKuis.kt
+++ HasilKuis.kt
@@ -2,20 +2,28 @@ package com.example.lotsoquiz
 
 import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View // Tambahkan impor ini untuk menggunakan View
 import android.widget.Button
 import android.widget.TextView
 
 class HasilKuis : Activity() {
 
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hasilkuis)
-        
+
+        sharedPreferences = getSharedPreferences("AppPrefs", MODE_PRIVATE)
+        val isDarkMode = sharedPreferences.getBoolean("darkMode", false)
+        applyDarkMode(isDarkMode)
+
         val benar = intent.getIntExtra("benar", 0)
         val salah = intent.getIntExtra("salah", 0)
         val hasil = intent.getIntExtra("hasil", 0)
-        
+
         val hasilTextView = findViewById<TextView>(R.id.hasil)
         val nilaiTextView = findViewById<TextView>(R.id.nilai)
 
@@ -25,14 +33,22 @@ class HasilKuis : Activity() {
         """.trimIndent()
 
         nilaiTextView.text = hasil.toString()
-        
+
         val btnUlangi = findViewById<Button>(R.id.ulangi)
         btnUlangi.setOnClickListener { ulangi() }
+    }
+
+    private fun applyDarkMode(isDarkMode: Boolean) {
+        if (isDarkMode) {
+            findViewById<View>(R.id.resultLayout).setBackgroundColor(resources.getColor(android.R.color.black))
+        } else {
+            findViewById<View>(R.id.resultLayout).setBackgroundColor(resources.getColor(R.color.pink))
+        }
     }
 
     private fun ulangi() {
         val intent = Intent(applicationContext, IntroductionActivity::class.java)
         startActivity(intent)
-        finish() 
+        finish()
     }
 }
